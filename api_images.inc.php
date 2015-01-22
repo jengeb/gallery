fc42cd2982e87cfc1714f25266bcade0ea36c096
Jennifer Gebske <jennifer@gebske.de>
2015-01-22 18:29:45 +0100

    API and templates

---
+++ api_images.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+function init_api_images($app) {
+
+// -----------------
+// GET
+// -----------------
+
+  $app->get("/images", function () use ($app) {
+
+    $dir = "Images/";
+
+    $files = scandir($dir);
+    foreach ($files as $file) {
+      if (filetype($dir . $file) == "file" && ($file != ".") && ($file != "..") && (substr($file, -4) == ".JPG")) {
+        $imageNames[] = $dir . $file;
+      }
+    }
+
+    $obj = array("images" => $imageNames);
+
+    echo json_encode_utf8($obj);
+
+  });
+
+}
+?>
