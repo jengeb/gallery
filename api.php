6532c252d073abaf933f944890ed245ea7b3235e
Jennifer Gebske <jennifer@gebske.de>
2015-01-22 18:28:39 +0100

    API and modularization of code

--- api.php
+++ api.php
@@ -1,16 +1,43 @@
 <?php
-  $dir = "Images/";
 
-  $files = scandir($dir);
-  foreach ($files as $file) {
-    if (filetype($dir . $file) == "file" && ($file != ".") && ($file != "..") && (substr($file, -4) == ".JPG")) {
-      $imageNames[] = $file;
-    }
+  require "api_images.inc.php";
+
+  $app = new \Slim\Slim();
+  //Header wird für Antwort gesetzt (als JSON und utf8)
+  $app->response->headers->set("Content-Type", "application/json; charset=utf-8");
+
+  $app->response->headers->set("Cache-Control", "no-cache, must-revalidate"); // HTTP/1.1
+  $app->response->headers->set("Pragma", "no-cache");
+  $app->response->headers->set("Expires", "Tue, 01 Jan 2000 00:00:00 GMT");
+
+  function json_response($code, $message) {
+    $err = array("code" => $code, "message" => $message);
+    return json_encode_utf8($err);
   }
 
-  for ($i = 0; $i < sizeof($imageNames); $i++) {
-    echo $imageNames[$i];
-    echo ", ";
+  function error($code, $message) {
+    http_response_code($code);
+    echo json_response($code, $message);
+    exit();
+  }
+
+  function success($code, $message) {
+    http_response_code($code);
+    echo json_response($code, $message);
+    exit();
+  }
+
+  function json_encode_utf8($obj) {
+    return json_encode($obj, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT) . "\n";
+  }
+
+  init_api_images($app);
+
+  try {
+    $app->run();
+  }
+  catch (Exception $e) {
+    error(500, "Unerwarteter Fehler: " . $e->getMessage());
   }
 
 ?>
--- js/gallery.js
+++ js/gallery.js
@@ -1,30 +1,50 @@
-angular.module('gallery', ['ngAnimate'])
-    .controller('MainController', function ($scope) {
-      $scope.slides = [
-      {image: 'Images/IMG_0805.JPG', text: 'Image 05'},
-      {image: 'Images/IMG_0806.JPG', text: 'Image 06'},
-      {image: 'Images/IMG_0808.JPG', text: 'Image 08'},
-      {image: 'Images/IMG_0809.JPG', text: 'Image 09'},
-      {image: 'Images/IMG_0810.JPG', text: 'Image 10'},
-      {image: 'Images/IMG_0811.JPG', text: 'Image 11'}
-      ];
+//Setup Angular
+var gallery = angular.module('gallery', ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
 
-      $scope.currentIndex = 0;
+gallery.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/start.html'
+    })
+    .when('/images', {
+      templateUrl: 'templates/images.html',
+    })
+    ;
+});
 
-      $scope.setCurrentSlideIndex = function (index) {
-          $scope.currentIndex = index;
-      };
+gallery.controller('MainController', function ($scope, $http) {
 
-      $scope.isCurrentSlideIndex = function (index) {
-          return $scope.currentIndex === index;
-      };
+  $scope.slides = [];
+  $scope.updateSlides = function() {
+      $http.get('api.php/images').then(function(response) {
+        $scope.slides = response.data.images;
+      });
+    };
 
-      $scope.previousSlide = function () {
-          $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? (++ $scope.currentIndex) : 0;
-      };
+  // $s{cope.slides = [
+  // {image: 'Images/IMG_0805.JPG', text: 'Image 05'},
+  // {image: 'Images/IMG_0806.JPG', text: 'Image 06'},
+  // {image: 'Images/IMG_0808.JPG', text: 'Image 08'},
+  // {image: 'Images/IMG_0809.JPG', text: 'Image 09'},
+  // {image: 'Images/IMG_0810.JPG', text: 'Image 10'},
+  // {image: 'Images/IMG_0811.JPG', text: 'Image 11'}
+  // ];}
 
-      $scope.nextSlide = function () {
-          $scope.currentIndex = ($scope.currentIndex > 0) ? (-- $scope.currentIndex) : ($scope.slides.length - 1);
-      };
+  $scope.currentIndex = 0;
 
-    });
+  $scope.setCurrentSlideIndex = function (index) {
+      $scope.currentIndex = index;
+  };
+
+  $scope.isCurrentSlideIndex = function (index) {
+      return $scope.currentIndex === index;
+  };
+
+  $scope.previousSlide = function () {
+      $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? (++ $scope.currentIndex) : 0;
+  };
+
+  $scope.nextSlide = function () {
+      $scope.currentIndex = ($scope.currentIndex > 0) ? (-- $scope.currentIndex) : ($scope.slides.length - 1);
+  };
+});
