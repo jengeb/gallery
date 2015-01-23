da71ed9e963316d6f9b6fa614024376b3cb5c4fd
Jennifer Gebske <jennifer@gebske.de>
2015-01-23 17:51:42 +0100

    API auth

---
+++ api_auth.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+
+function init_api_auth($app) {
+
+  $app->post("/auth", function () use ($app) {
+    $json = $app->request->getBody();
+    $obj = json_decode($json, true);
+
+    if (is_string($obj["Username"]) && is_string($obj["Password"])) {
+      if (($obj["Username"] == "testuser") && ($obj["Password"] == "testpw")) {
+        session_start();
+        $_SESSION["Username"] = $obj["Username"];
+          $app -> halt(200);
+      } else {
+          $app -> halt(401);
+      }
+    }
+  });
+
+}
+?>
