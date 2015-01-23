00920520648545b239ef0ca6a39890a8eb46d8da
Jennifer Gebske <jennifer@gebske.de>
2015-01-23 01:10:04 +0100

    API post

--- api_images.inc.php
+++ api_images.inc.php
@@ -1,18 +1,15 @@
 <?php
 
 function init_api_images($app) {
-
 // -----------------
 // GET
 // -----------------
 
   $app->get("/images", function () use ($app) {
-
     $dir = "Images/";
-
     $files = scandir($dir);
     foreach ($files as $file) {
-      if (filetype($dir . $file) == "file" && ($file != ".") && ($file != "..") && (substr($file, -4) == ".JPG")) {
+      if (filetype($dir . $file) == "file" && ($file != ".") && ($file != "..")) {
         $imageNames[] = array("image" => $dir . $file);
       }
     }
@@ -22,5 +19,21 @@ function init_api_images($app) {
     echo json_encode_utf8($obj);
   });
 
+
+// -----------------
+// POST -> insert
+// -----------------
+
+  $app->post("/images", function () use ($app) {
+    $dir = "Images/";
+
+    $src = $_FILES['upfile']['tmp_name'];
+    $dest = $dir.$_FILES['upfile']['name'];
+
+    move_uploaded_file($src, $dest);
+    $app->response->redirect("../#/images");
+  });
+
 }
+
 ?>
